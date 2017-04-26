02669b075bb68e99a46f898c337ab9e7eabb8b65
Tucker Tibbetts <ctibbetts@novetta.com>
2017-04-26 11:47:25 -0400

    go program to wait for postgres

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pg-isready
-Wait for Postgres Server
+
+Simple app that waits for a postgres connection before exiting.
+
+### Environment Variables
+```
+POSTGRES_USER - Username
+POSTGRES_PASS - Password
+POSTGRES_HOST - Hostname or IP
+POSTGRES_PORT - Port Number
+POSTGRES_DB   - Database name
+```
+
+# Vendoring
+`pg-go` uses [govendor](https://github.com/kardianos/govendor) for vendoring.
+To pull the vendor files, run:
+```
+go get -u github.com/kardianos/govendor
+govendor sync
+```
+
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/stdlib"
+)
+
+const sleep = 3
+
+func env(variable string, defaultValue string) string {
+	out := os.Getenv(variable)
+	if out == "" {
+		out = defaultValue
+	}
+	return out
+}
+
+func main() {
+	host := env("POSTGRES_HOST", "localhost")
+	port := env("POSTGRES_PORT", "5432")
+	user := env("POSTGRES_USER", "postgres")
+	pass := env("POSTGRES_PASS", "password")
+	db := env("POSTGRES_DB", "postgres")
+
+	dbinfo := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		user, pass, host, port, db)
+	fmt.Printf("Connecting to: %s\n", dbinfo)
+
+	connected := false
+	for !connected {
+		db, err := sql.Open("pgx", dbinfo)
+		if err != nil {
+			fmt.Println("Can't connect to database...")
+			time.Sleep(sleep * time.Second)
+			continue
+		}
+
+		err = db.Ping()
+		if err != nil {
+			fmt.Println("Can't ping database...")
+			time.Sleep(sleep * time.Second)
+			continue
+		}
+		connected = true
+	}
+	fmt.Println("Connected")
+}
